88dc4dc58b899bd1b4bdc26cd7f04bdab15d3ae7
admin <admin@DESKTOP-58TVSAQ>
2022-03-24 22:04:08 +0530

    commit 56

--- com.crm.vtiger/src/test/java/com/crm/vtigerpractice/createDemoTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtigerpractice/createDemoTest.java
@@ -7,6 +7,7 @@ public class createDemoTest {
 public void createOrg() {
 	System.out.println("user1 org created");
 	System.out.println("user2 new org created");
+	System.out.println("user3 new org created");
 }
 
 
