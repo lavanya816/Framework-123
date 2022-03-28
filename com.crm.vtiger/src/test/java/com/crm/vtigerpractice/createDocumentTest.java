ae212874085e4be9f4ba98b869bf678ce849d026
Admin <Admin@LAPTOP-6751OT4L>
2022-03-28 14:53:47 +0530

    modified all test scripts

--- com.crm.vtiger/src/test/java/com/crm/vtigerpractice/createDocumentTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtigerpractice/createDocumentTest.java
@@ -10,15 +10,8 @@ public class createDocumentTest {
 		System.out.println("Document modified");
 		System.out.println("document verifeid");
 	}
-	
-	
-	
-	
-	
-	
 	@Test
 	public void modifyDocument() {
 		System.out.println("Modify Document");
 	}
-
 }
--- com.crm.vtiger/src/test/java/com/crm/vtigerpractice/loginToVtigerParametersTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtigerpractice/loginToVtigerParametersTest.java
@@ -1,40 +1,41 @@
-package com.crm.vtigerpractice;
-
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class loginToVtigerParametersTest {
-@Test 
-public void login()
-{
-	WebDriver driver =null;
-	String browser=System.getProperty("browser");
-	String url=System.getProperty("url");
-	String username=System.getProperty("username");
-	String password=System.getProperty("password");
-	
-	if (browser.equals("chrome")) {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	} else if (browser.equals("firefox")) {
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
-	}
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	// step-1: launch v-tiger and login
-	driver.get(url);
-	driver.findElement(By.name("user_name")).sendKeys(username);
-	driver.findElement(By.name("user_password")).sendKeys(password);
-	driver.findElement(By.id("submitButton")).click();	
-	driver.quit();
-}
-}
+//package com.crm.vtigerpractice;
+//
+//
+//import java.util.concurrent.TimeUnit;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.Test;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//
+//public class loginToVtigerParametersTest {
+//@Test 
+//public void login()
+//{
+//	WebDriver driver =null;
+//	String browser=System.getProperty("browser");
+//	String url=System.getProperty("url");
+//	String username=System.getProperty("username");
+//	String password=System.getProperty("password");
+//	
+//	if (browser.equals("chrome")) {
+//		WebDriverManager.chromedriver().setup();
+//		driver = new ChromeDriver();
+//	} else if (browser.equals("firefox")) {
+//		WebDriverManager.firefoxdriver().setup();
+//		driver = new FirefoxDriver();
+//	}
+//	
+//	driver.manage().window().maximize();
+//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//	// step-1: launch v-tiger and login
+//	driver.get(url);
+//	driver.findElement(By.name("user_name")).sendKeys(username);
+//	driver.findElement(By.name("user_password")).sendKeys(password);
+//	driver.findElement(By.id("submitButton")).click();	
+//	driver.quit();
+//}
+//}
